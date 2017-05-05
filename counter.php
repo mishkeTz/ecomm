dcf42828c55067e49f468774638ef6b0bc3e784d
Nikola Misic <mishke.deejay@gmail.com>
2017-05-05 19:33:55 +0200

    Delete counter.php

--- counter.php
+++
@@ -1,5 +0,0 @@
-<?php  
-
-echo strlen("We would like to welcome all our new members from other groups and servers! We hope you enjoy the new group hall. Please remember to upgrade your group skills to the current max of level 45.");
-
-
